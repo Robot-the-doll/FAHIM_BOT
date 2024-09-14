3ab20b21a306e5db1b1a01435abd3fd48a0389bb
Robot-the-doll <fahim.chowdury.143@gmail.com>
2024-09-14 08:38:49 +0600

    Update chatgpt.js

--- scripts/commands/chatgpt.js
+++ scripts/commands/chatgpt.js
@@ -1,10 +1,10 @@
 module.exports.config = {
-  name: "nayan",
+  name: "fahim",
 	version: "1.0.0",
 	permission: 0,
   prefix: false,
-	credits: "Nayan",
-	description: "I am nayan better than ChatGPT and Bard",
+	credits: "Fahim",
+	description: "I am fahim better than ChatGPT and Bard",
 	category: "user",
 	cooldowns: 5
 };
